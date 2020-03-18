15e3dcd71fd0b4c6f29a4b80f51a4f06a657dc9f
KJH <jinhungkim@hanmail.net>
2020-03-19 04:19:19 +0900

    Setting DateSelectedListener

--- app/src/main/java/io/yagus/squarecalendar/MainActivity.java
+++ app/src/main/java/io/yagus/squarecalendar/MainActivity.java
@@ -4,39 +4,75 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.squareup.timessquare.CalendarPickerView;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 import static com.squareup.timessquare.CalendarPickerView.SelectionMode.RANGE;
 
 public class MainActivity extends AppCompatActivity {
+
+    CalendarPickerView calendarView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         Calendar nextYear = Calendar.getInstance();
         nextYear.add(Calendar.YEAR, 1);
 
-        CalendarPickerView calendar = (CalendarPickerView) findViewById(R.id.calendar_view);
+        calendarView = (CalendarPickerView) findViewById(R.id.calendar_view);
         Date today = new Date();
-        calendar.init(today, nextYear.getTime())
+        calendarView.init(today, nextYear.getTime())
                 .inMode(RANGE);
 
-        calendar.setOnDateSelectedListener(new CalendarPickerView.OnDateSelectedListener() {
+        calendarView.setOnDateSelectedListener(new CalendarPickerView.OnDateSelectedListener() {
             @Override
             public void onDateSelected(Date date) {
-                Toast.makeText(getApplicationContext(),"Selected:"+date,Toast.LENGTH_SHORT).show();
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(date);
+                String str = String.format(Locale.getDefault(),"Selected : %d %02d %02d",
+                        calendar.get(Calendar.YEAR),
+                        calendar.get(Calendar.MONTH)+1,
+                        calendar.get(Calendar.DATE));
+
+                Toast.makeText(getApplicationContext(),str,Toast.LENGTH_SHORT).show();
+
+                button.setEnabled(calendarView.getSelectedDates().size() > 1);
+//                dateFinish();
             }
 
             @Override
             public void onDateUnselected(Date date) {
-                Toast.makeText(getApplicationContext(),"Unselected:"+date,Toast.LENGTH_SHORT).show();
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(date);
+                String str = String.format(Locale.getDefault(),"Deselected : %d %02d %02d",
+                        calendar.get(Calendar.YEAR),
+                        calendar.get(Calendar.MONTH)+1,
+                        calendar.get(Calendar.DATE));
+                Toast.makeText(getApplicationContext(),str,Toast.LENGTH_SHORT).show();
+                button.setEnabled(calendarView.getSelectedDates().size() > 1);
+            }
+        });
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(calendarView.getSelectedDates().size() == 2){
+                    Toast.makeText(getApplicationContext(),"FINISH",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),"날짜 2개를 입력해주세요.",Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
